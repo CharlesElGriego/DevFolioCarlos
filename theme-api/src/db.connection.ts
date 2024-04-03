97914743c01648af7692a36be39c88ea6c34fb34
Charles <cnavarro3196@protonmail.com>
2024-04-02 23:43:15 -0600

    update db

--- theme-api/src/db.connection.ts
+++ theme-api/src/db.connection.ts
@@ -1,7 +1,7 @@
 const Datastore = require('nedb-promises');
 
 import DataStoreType from 'nedb-promises';
-const themeDb: DataStoreType = new Datastore({
+const themeDb: DataStoreType<any> = new Datastore({
   filename: './theme.data.db',
   autoload: true,
 });
