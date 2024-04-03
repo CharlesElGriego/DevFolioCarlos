de9dd8b9a1cfcde78ba9246b2098c2ead9b78f6b
Charles <cnavarro3196@protonmail.com>
2024-04-02 22:58:52 -0600

    feat: add route to editor

--- feDevFolioCarlos/src/app/app.routes.ts
+++ feDevFolioCarlos/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ThemeEditorComponent } from './theme-editor/theme-editor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/theme-editor', pathMatch: 'full' },
+  { path: 'theme-editor', component: ThemeEditorComponent },
+];
