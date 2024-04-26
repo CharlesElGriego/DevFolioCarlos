272f13c18d59c316cbd9fc4954bcc330208c045c
Charles <cnavarro3196@protonmail.com>
2024-04-25 19:37:42 -0600

    feat: add ngrx

--- charles-image-viewer/src/app/app.config.ts
+++ charles-image-viewer/src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
